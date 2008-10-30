b8d42dd73cc3fc75167814754fea0346645b3ff0
Johannes J. Schmidt <jo@jolap.(none)>
2008-10-30 21:21:34 +0100

    splitted value into value and unit

--- lib/css/value.rb
+++ lib/css/value.rb
@@ -2,8 +2,9 @@ module Css
   # The Value holds a CSS value and a references to its parent Property 
   # (which references its parent Element, which itself references its parent Stylesheet)
   class Value
-    attr_reader :name, :property
+    attr_reader :value, :unit, :property
   
+    UNITS = %w{pt pc in mm cm px em ex %}
     TYPES = {
       'color' => [:color],
       'background' => [:color, :url, :repeat, :positiony, :positionx],
@@ -22,25 +23,29 @@ module Css
       'line-height' => [:unit],
     }
   
-    # Creates a new Value by name and parent property.
-    def initialize(name = '', property = Property.new)
-      @name = name.strip
+    # Creates a new Value by value and parent property.
+    def initialize(value = '', property = Property.new)
+      @value = value.strip
+      if value =~ /\d+(#{UNITS.join('|')})$/
+        @unit = @value.gsub(/^\d+(#{UNITS.join('|')})$/, '\1')
+        @value.gsub!(/(#{UNITS.join('|')})$/, '')
+      end
       @property = property
     end
   
     # Returns the type of the value.
-    # Try to fetch it from the TYPES hash by property's name and the index of the current value from property's values.
+    # Try to fetch it from the TYPES hash by property's value and the index of the current value from property's values.
     # Returns :text if none was found.
     def type
-      TYPES[property.name][property.values.index(self)]
+      TYPES[property.value][property.values.index(self)]
     rescue => e
       :text
     end
   
     # Returns a String representation of the value.
-    # That is just the name.
+    # That is just the value.
     def to_s
-      name
+      [value, unit].join
     end
   end
 end
